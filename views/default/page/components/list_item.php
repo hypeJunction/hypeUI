71466239f60bab403062edd75d8448487868d807
Steve Clay <steve@mrclay.org>
2017-09-28 17:28:51 -0400

    feat(views): allow specifying card view outside galleries (#8)

--- views/default/page/components/list_item.php
+++ views/default/page/components/list_item.php
@@ -6,7 +6,7 @@ $is_gallery = elgg_in_context('gallery');
 
 if ($full) {
 	echo elgg_view('object/elements/full', $vars);
-} else if ($is_gallery) {
+} else if ($is_gallery || !empty($vars['card'])) {
 	if (elgg_extract('card', $vars, false)) {
 		echo elgg_view('object/elements/card', $vars);
 	} else {
