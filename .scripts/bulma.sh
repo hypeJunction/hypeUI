b6fae05f2f9eeb21fe516afb12661074e7797930
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2017-11-28 17:15:35 +0300

    feat(scss): drop server-side scss compilation

--- .scripts/bulma.sh
+++ .scripts/bulma.sh
@@ -5,13 +5,13 @@
 # This script copies Bulma source files and converts them to .scss
 
 #convert .sass files to scss files:
-sass-convert -R --from sass --to scss ./node_modules/bulma/sass/ ./views/default/bulma/
-sass-convert --from sass --to scss ./node_modules/bulma/bulma.sass ./views/default/bulma/bulma.scss
+sass-convert -R --from sass --to scss ./node_modules/bulma/sass/ ./sass/bulma/
+sass-convert --from sass --to scss ./node_modules/bulma/bulma.sass ./sass/bulma/bulma.scss
 
 # replace local @imports what include the .sass extension
-find ./views/default/bulma/ -name "*.scss" | xargs sed -i "s/\.sass//g"
-find ./views/default/bulma/ -name "*.scss" | xargs sed -i "s/sass\///g"
+find ./sass/bulma/ -name "*.scss" | xargs sed -i "s/\.sass//g"
+find ./sass/bulma/ -name "*.scss" | xargs sed -i "s/sass\///g"
 
 
 #ensure that we got them all
-grep -R "@import" ./views/default/bulma/
+grep -R "@import" ./sass/bulma/
--- Gruntfile.js
+++ Gruntfile.js
@@ -1,159 +1,179 @@
 module.exports = function (grunt) {
 
-	var package = grunt.file.readJSON('package.json');
+    var package = grunt.file.readJSON('package.json');
 
-	// Project configuration.
-	grunt.initConfig({
-		pkg: package,
-		// Bump version numbers
-		version: {
-			pkg: {
-				src: ['package.json', 'composer.json'],
-			},
-			manifest: {
-				options: {
-					pkg: grunt.file.readJSON('package.json'),
-					prefix: '\<version type=\"dist\"\>'
-				},
-				src: ['manifest.xml'],
-			}
-		},
-		clean: {
-			release: {
-				src: ['build/', 'releases/', 'mod/', 'vendor/', 'composer.lock']
-			}
-		},
-		copy: {
-			release: {
-				src: [
-					'**',
-					'!**/.git*',
-					'!releases/**',
-					'!build/**',
-					'!mod/**',
-					'!node_modules/**',
-					'!package.json',
-					'!config.rb',
-					'!sass/**',
-					'!tests/**',
-					'!composer.json',
-					'!composer.lock',
-					'!package.json',
-					'!phpunit.xml',
-					'!Gruntfile.js',
-					'!Gemfile',
-					'!Gemfile.lock'
-				],
-				dest: 'build/',
-				expand: true
-			},
-		},
-		compress: {
-			release: {
-				options: {
-					archive: 'releases/<%= pkg.name %>-<%= pkg.version %>.zip'
-				},
-				cwd: 'build/',
-				src: ['**/*'],
-				dest: '<%= pkg.name %>/',
-				expand: true
-			}
-		},
-		gitcommit: {
-			release: {
-				options: {
-					message: 'chore(build): release <%= pkg.version %>',
-				},
-				files: {
-					src: ["composer.json", "manifest.xml", "package.json", "CHANGELOG.md"],
-				}
-			},
-		},
-		gitfetch: {
-			release: {
-				all: true
-			}
-		},
-		gittag: {
-			release: {
-				options: {
-					tag: '<%= pkg.version %>',
-					message: 'Release <%= pkg.version %>'
-				}
-			}
-		},
-		gitpush: {
-			release: {
-			},
-			release_tags: {
-				options: {
-					tags: true
-				}
-			}
-		},
-		gh_release: {
-			options: {
-				token: process.env.GITHUB_TOKEN,
-				repo: package.repository.repo,
-				owner: package.repository.owner
-			},
-			release: {
-				tag_name: '<%= pkg.version %>',
-				name: 'Release <%= pkg.version %>',
-				body: grunt.file.read('release.md'),
-				draft: false,
-				prerelease: false,
-				asset: {
-					name: '<%= pkg.name %>-<%= pkg.version %>.zip',
-					file: 'releases/<%= pkg.name %>-<%= pkg.version %>.zip',
-					'Content-Type': 'application/zip'
-				}
-			}
-		},
-		conventionalChangelog: {
-			options: {
-				changelogOpts: {
-					// conventional-changelog options go here
-					preset: 'angular'
-				}
-			},
-			release: {
-				src: 'CHANGELOG.md'
-			}
+    // Project configuration.
+    grunt.initConfig({
+        pkg: package,
+        // Bump version numbers
+        version: {
+            pkg: {
+                src: ['package.json', 'composer.json'],
+            },
+            manifest: {
+                options: {
+                    pkg: grunt.file.readJSON('package.json'),
+                    prefix: '\<version type=\"dist\"\>'
+                },
+                src: ['manifest.xml'],
+            }
+        },
+        clean: {
+            release: {
+                src: ['build/', 'releases/', 'mod/', 'vendor/', 'composer.lock']
+            }
+        },
+        copy: {
+            release: {
+                src: [
+                    '**',
+                    '!**/.git*',
+                    '!releases/**',
+                    '!build/**',
+                    '!mod/**',
+                    '!node_modules/**',
+                    '!package.json',
+                    '!config.rb',
+                    '!sass/**',
+                    '!tests/**',
+                    '!composer.json',
+                    '!composer.lock',
+                    '!package.json',
+                    '!phpunit.xml',
+                    '!Gruntfile.js',
+                    '!Gemfile',
+                    '!Gemfile.lock'
+                ],
+                dest: 'build/',
+                expand: true
+            },
+        },
+        compress: {
+            release: {
+                options: {
+                    archive: 'releases/<%= pkg.name %>-<%= pkg.version %>.zip'
+                },
+                cwd: 'build/',
+                src: ['**/*'],
+                dest: '<%= pkg.name %>/',
+                expand: true
+            }
+        },
+        gitcommit: {
+            release: {
+                options: {
+                    message: 'chore(build): release <%= pkg.version %>',
+                },
+                files: {
+                    src: ["composer.json", "manifest.xml", "package.json", "CHANGELOG.md"],
+                }
+            },
+        },
+        gitfetch: {
+            release: {
+                all: true
+            }
+        },
+        gittag: {
+            release: {
+                options: {
+                    tag: '<%= pkg.version %>',
+                    message: 'Release <%= pkg.version %>'
+                }
+            }
+        },
+        gitpush: {
+            release: {},
+            release_tags: {
+                options: {
+                    tags: true
+                }
+            }
+        },
+        gh_release: {
+            options: {
+                token: process.env.GITHUB_TOKEN,
+                repo: package.repository.repo,
+                owner: package.repository.owner
+            },
+            release: {
+                tag_name: '<%= pkg.version %>',
+                name: 'Release <%= pkg.version %>',
+                body: grunt.file.read('release.md'),
+                draft: false,
+                prerelease: false,
+                asset: {
+                    name: '<%= pkg.name %>-<%= pkg.version %>.zip',
+                    file: 'releases/<%= pkg.name %>-<%= pkg.version %>.zip',
+                    'Content-Type': 'application/zip'
+                }
+            }
+        },
+        conventionalChangelog: {
+            options: {
+                changelogOpts: {
+                    // conventional-changelog options go here
+                    preset: 'angular'
+                }
+            },
+            release: {
+                src: 'CHANGELOG.md'
+            }
+        },
+        sass: {
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: 'sass',
+                    src: ['*.scss'],
+                    dest: '../views/default',
+                    ext: '.css'
+                }]
+            }
+        },
+        watch: {
+            css: {
+                files: 'sass/**/*.scss',
+                tasks: ['sass']
+            }
+        }
+    });
+    // Load all grunt plugins here
+    grunt.loadNpmTasks('grunt-version');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-compress');
+    grunt.loadNpmTasks('grunt-composer');
+    grunt.loadNpmTasks('grunt-conventional-changelog');
+    grunt.loadNpmTasks('grunt-git');
+    grunt.loadNpmTasks('grunt-gh-release');
 
-		}
-	});
-	// Load all grunt plugins here
-	grunt.loadNpmTasks('grunt-version');
-	grunt.loadNpmTasks('grunt-contrib-copy');
-	grunt.loadNpmTasks('grunt-contrib-clean');
-	grunt.loadNpmTasks('grunt-contrib-compress');
-	grunt.loadNpmTasks('grunt-composer');
-	grunt.loadNpmTasks('grunt-conventional-changelog');
-	grunt.loadNpmTasks('grunt-git');
-	grunt.loadNpmTasks('grunt-gh-release');
+    grunt.registerTask('readpkg', 'Read in the package.json file', function () {
+        grunt.config.set('pkg', grunt.file.readJSON('package.json'));
+    });
 
-	grunt.registerTask('readpkg', 'Read in the package.json file', function () {
-		grunt.config.set('pkg', grunt.file.readJSON('package.json'));
-	});
+    // Release task
+    grunt.registerTask('release', function (n) {
+        var n = n || 'patch';
+        grunt.task.run([
+            'version::' + n,
+            'readpkg',
+            'conventionalChangelog:release',
+            'gitfetch:release',
+            'gitcommit:release',
+            'gittag:release',
+            'gitpush:release',
+            'gitpush:release_tags',
+            'clean:release',
+            'composer:install:no-dev:prefer-dist',
+            'copy:release',
+            'compress:release',
+            'gh_release',
+        ]);
+    });
 
-	// Release task
-	grunt.registerTask('release', function (n) {
-		var n = n || 'patch';
-		grunt.task.run([
-			'version::' + n,
-			'readpkg',
-			'conventionalChangelog:release',
-			'gitfetch:release',
-			'gitcommit:release',
-			'gittag:release',
-			'gitpush:release',
-			'gitpush:release_tags',
-			'clean:release',
-			'composer:install:no-dev:prefer-dist',
-			'copy:release',
-			'compress:release',
-			'gh_release',
-		]);
-	});
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('build', ['sass']);
 };
