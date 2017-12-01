aa0c4a345f19df219b2c503606475fbb28a5f11a
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2017-12-01 16:59:36 +0100

    feat(members): use proper filter on members page

---
+++ views/default/resources/members/index.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Members index
+ */
+
+// The URL segment after members/
+$page = elgg_extract('page', $vars);
+
+$tabs = elgg_trigger_plugin_hook('members:config', 'tabs', null, array());
+
+foreach ($tabs as $type => $values) {
+	$tabs[$type]['name'] = $type;
+	$tabs[$type]['text'] = $tabs[$type]['title'];
+	$tabs[$type]['selected'] = ($page == $type);
+}
+
+$params = array(
+	'options' => array('type' => 'user', 'full_view' => false),
+);
+
+$content = elgg_trigger_plugin_hook('members:list', $page, $params, null);
+if ($content === null) {
+	forward('', '404');
+}
+
+$title = elgg_echo("members:title:{$page}");
+
+$params = array(
+	'content' => $content,
+	'sidebar' => elgg_view('members/sidebar'),
+	'title' => $title,
+	'filter' => $tabs,
+);
+
+$body = elgg_view_layout('content', $params);
+
+echo elgg_view_page($title, $body);
