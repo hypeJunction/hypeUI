a38b6a07cb07ddd5b90c37d36482b5e5aac1d27c
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2017-12-02 12:08:22 +0100

    fix(output): do not render body if it is empty

--- views/default/object/elements/summary/body.php
+++ views/default/object/elements/summary/body.php
@@ -3,6 +3,11 @@
 $content = elgg_view('object/elements/summary/content', $vars);
 $attachments = elgg_view('object/elements/summary/attachments', $vars);
 
+$body = $content . $attachments;
+if (empty($body)) {
+	return;
+}
+
 echo elgg_format_element('div', [
 	'class' => 'elgg-listing-summary-body',
-], $content . $attachments);
+], $body);
