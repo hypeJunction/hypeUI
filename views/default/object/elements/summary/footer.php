59281a5737837b297d9268e2f59c9bbc21765505
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2017-12-02 12:14:11 +0100

    fix(output): do not render footer if it is empty

--- views/default/object/elements/summary/footer.php
+++ views/default/object/elements/summary/footer.php
@@ -3,6 +3,11 @@
 $metadata = elgg_view('object/elements/summary/metadata', $vars);
 $responses = elgg_view('object/elements/summary/responses', $vars);
 
+$footer = $metadata . $responses;
+if (empt($footer)) {
+	return;
+}
+
 echo elgg_format_element('div', [
 	'class' => 'elgg-listing-summary-footer',
-], $metadata . $responses);
+], $footer);
